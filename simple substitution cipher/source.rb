959991a400ba5b1900c8e6a01e880491f424d293
StanislavBerkovich <stanislav.berkovich@gmail.com>
2015-02-12 22:03:00 +0300

    Add simple substitution cipher

--- simple substitution cipher/source.rb
+++ simple substitution cipher/source.rb
@@ -1,2 +1,17 @@
 #simple substitution cipher
+require 'yaml'
+ 
+def substitution_with_key text, key
+	text.chars.inject('') { |res, ch| res + key[ch] }
+end
+
+
+input_text = File.open('in.txt'){ |file| file.read }.chop
+crypt_key = YAML::load_file('key.txt')
+
+crypt_text = substitution_with_key(input_text, crypt_key) 
+File.open('crypt.txt', 'w'){|file| file.write crypt_text }
+
+encrypt_key = crypt_key.invert
+File.open('encrypt.txt', 'w') {|file| file.write substitution_with_key(crypt_text, encrypt_key)}
 
